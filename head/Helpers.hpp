87e1ae8828f0ce473d93d0a039889fd13378e093
Facinet Kouyate <fakouyat@student.42wolfsburg.de>
2023-02-24 13:59:12 +0100

    done

--- head/Helpers.hpp
+++ head/Helpers.hpp
@@ -16,7 +16,27 @@
 # include <fstream>
 # include <vector>
 # include <map>
-
+# define OPEN_SYMBOL '{'
+# define CLOSE_SYMBOL '}'
+# define SEMI_COLUMN ';'
+# define KEY_SERVER "server"
+# define KEY_SERVER_LEN 6
+# define SPACE ' '
+# define KEY_LISTEN "listen"
+# define KEY_SERV_NAME "server_name"
+# define KEY_ERR_PAGE "error_page"
+# define KEY_MAX_BODY_SIZE "client_max_body_size"
+# define KEY_ROUTE "route"
+# define KEY_METHOD "method"
+# define KEY_DIRECTORY_LISTING "directory_listing"
+# define KEY_ROOT "root"
+# define KEY_INDEX "index"
+# define KEY_UPLOAD "upload"
+# define KEY_REDIRECT "redirect"
+# define KEY_CGI_EXT "cgi_ext"
+# define KEY_CGI_PASS "cgi_pass"
+# define KEY_HOST "host"
+# define TAB 9
 typedef struct	s_route {
 	std::vector<std::string> 	method;
 	bool						directory_listing;
@@ -37,8 +57,9 @@ typedef struct s_params {
 	int									client_max_body_size;
 	std::map<std::string, route_t>		routes;
 	std::vector<std::string>			files_extensions;
-	
-	
+	int									start_data;
+	int									end_data;
+
 }				params_t;
 
 # include "ServerConf.hpp"
@@ -51,10 +72,12 @@ class	Helpers {
 		static void						parse_file(ServerConf* servconf, 
 														const std::string & fileconf);
 		static std::vector<std::string>	get_config_data(const std::string & fileconf);
-		static int						count_servers(const std::vector<std::string> & config_data);
+		static int						count_servers(const std::vector<std::string> & config_data, ServerConf* servconf);
 		static void						skipe_spaces(const std::string & str, size_t* cursor);
 		static int						find_close_symbol(const std::vector<std::string> & config_data, 
 														  size_t* cursor, int level);
+		static	void					skipe_empty_line(const std::vector<std::string> & config_data, size_t* cursor);
+		static	int						fill_host_value(std::string line, params_t* serv, size_t* cursor);
 };
 
 #endif
--- srcs/Helpers.cpp
+++ srcs/Helpers.cpp
@@ -39,47 +39,81 @@ void	Helpers::parse_file(ServerConf* servconf,\
 	std::vector<std::string>	data;
 
 	data = get_config_data(fileconf);
-	nb_server = count_servers(data);
-	std::cout << "COUNT SERVER" << nb_server << "\n";
+	nb_server = count_servers(data, servconf);
+	size_t j;
 	if (nb_server < 0)
 	{
 		servconf->setValidation(false);
 		return ;
 	}
+	for (size_t i = 0; i < nb_server; i++)
+	{
+		std::cout << "**********SERVER*******************\n";
+		while (servconf->servers[i].start_data <= servconf->servers[i].end_data)
+		{
+			j = 0;
+			skipe_spaces(data[servconf->servers[i].start_data], &j);
+			if (data[servconf->servers[i].start_data].substr(j, 4) == KEY_HOST)
+			{
+				if (fill_host_value(data[servconf->servers[i].start_data], &servconf->servers[i], &j) < 0)
+				{
+					servconf->setValidation(false);
+					return ;
+				}
+			}
+			servconf->servers[i].start_data += 1;
+		}
+		std::cout << "HOST : " << servconf->servers[i].host << "\n";
+		std::cout << "*************************************\n";
+	}
 
 }
 
-int  Helpers::count_servers(const std::vector<std::string> & config_data)
+int  Helpers::count_servers(const std::vector<std::string> & config_data, ServerConf* servconf)
 {
 	int				nb = 0;
-	const			std::string key = "server";
-	const char		open_symbol = '{';
 	int				serv_open = -1;
 	size_t			data_len = config_data.size(), j;
 
 	for (size_t i = 0; i < data_len; i++)
 	{
-		j = key.size();
-		if (config_data[i].substr(0, j) == key)
+		j = KEY_SERVER_LEN;
+		skipe_empty_line(config_data, &i);
+		if (config_data[i] == "" && nb >= 1)
+			return (nb);
+		if (config_data[i].substr(0, j) == KEY_SERVER)
 		{
 			skipe_spaces(config_data[i], &j);
-			if (config_data[i][j] == open_symbol)
+			if (config_data[i][j] == '\0')
 			{
+				i += 1;
+				j = 0;
+			}
+			if (config_data[i][j] == OPEN_SYMBOL)
+			{
+				params_t param;
+				param.start_data = i += 1;
 				if (find_close_symbol(config_data, &i, 0) >= 0)
+				{
 					nb += 1;
+					param.end_data = i - 1;
+					servconf->servers.push_back(param);
+				}
 				else
 					return (-1);
 			}
 			else
 				return (-1);
 		}
+		else
+			return (-1);
 	}
 	return (nb);
 }
 
 void	Helpers::skipe_spaces(const std::string & str, size_t* cursor)
 {
-	while (str[*cursor] == ' ')
+	while (str[*cursor] == SPACE || str[*cursor] == TAB)
 		*cursor += 1;
 }
 
@@ -87,24 +121,62 @@ int Helpers::find_close_symbol(const std::vector<std::string> & config_data,\
 	size_t* cursor, int level)
 {
 	size_t				data_len = config_data.size(), j;
-	const char			close_symbole = '}';
-	const std::string   key = "server";
-
 	*cursor += 1;
 	while (*cursor < data_len)
 	{
 		j = 0;
-		if (config_data[*cursor].substr(0, key.size()) == key)
+		if (config_data[*cursor].substr(0, KEY_SERVER_LEN) == KEY_SERVER)
 			return (-1);
 		if (level > 0)
 			skipe_spaces(config_data[*cursor], &j);
-		if (config_data[*cursor][j] == close_symbole)
+		if (config_data[*cursor][j] == CLOSE_SYMBOL)
 		{
 			if (j + 1 == config_data[*cursor].size())
+				return (j);
+			j += 1;
+			skipe_spaces(config_data[*cursor], &j);
+			if (config_data[*cursor][j] == '\0')
 				return (j);
 			return (-1);
 		}
 		*cursor += 1;
 	}
 	return (-1);
+}
+
+
+void	Helpers::skipe_empty_line(const std::vector<std::string> & config_data, size_t* cursor)
+{
+	size_t len = config_data.size(), j;
+
+	while (*cursor < len)
+	{
+		if (config_data[*cursor] == "")
+			*cursor += 1;
+		else 
+		{
+			j = 0;
+			while (config_data[*cursor][j] == SPACE)
+				j++;
+			if (config_data[*cursor][j] != '\0')
+				return ;
+			*cursor += 1;
+		}
+	}
+}
+
+int	Helpers::fill_host_value(std::string line, params_t* serv, size_t* cursor)
+{
+	std::string	host = "";
+	*cursor += 4;
+	if (line[line.size() - 1] != SEMI_COLUMN)
+		return (-1);
+	skipe_spaces(line, cursor);
+	while (*cursor < line.size())
+	{
+		host += line[*cursor];
+		*cursor += 1;
+	}
+	std::cout << "HERE : " << host << "\n";
+	return(0);
 }
